35175e75cffc28f55fc40812e8a9d7a93bf61b41
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-04-13 11:40:30 -0600

    Install libraries

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasUlids, SoftDeletes, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
