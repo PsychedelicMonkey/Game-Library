d614bafc2e61feaca8609df03bd766bc90ffec7f
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-06-01 08:26:27 -0600

    Linting

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,11 +1,11 @@
-import {Head} from "@inertiajs/react";
+import { Head } from '@inertiajs/react';
 
 export default function Home() {
     return (
         <>
             <Head title="Home Page" />
 
-            <div className="p-4 lg:p-6 max-w-7xl mx-auto">
+            <div className="mx-auto max-w-7xl p-4 lg:p-6">
                 <h1 className="text-3xl font-semibold text-gray-900">Home Page</h1>
             </div>
         </>
