ef70d5ff4a8fe011ce23b0c485de5200016481d6
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-06-14 14:47:20 -0600

    Inertia

---
+++ resources/js/pages/home.jsx
@@ -0,0 +1,11 @@
+import { Head } from '@inertiajs/react';
+
+export default function Home(){
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Head title="Home Page" />
+
+            <h1 className="text-3xl font-semibold">Home Page</h1>
+        </div>
+    )
+}
