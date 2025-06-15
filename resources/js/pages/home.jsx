a8ce9d0f3720ad24d9cb0b74610b4fdfbc3911b3
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-06-15 09:09:37 -0600

    Inertia

---
+++ resources/js/pages/home.jsx
@@ -0,0 +1,11 @@
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return <div>
+        <Head title="Home Page" />
+
+        <div className="max-w-7xl mx-auto">
+            <h1 className="text-3xl font-semibold">Home Page</h1>
+        </div>
+    </div>
+}
---
+++ resources/js/ssr.jsx
@@ -0,0 +1,16 @@
+import { createInertiaApp } from '@inertiajs/react'
+import createServer from '@inertiajs/react/server'
+import ReactDOMServer from 'react-dom/server'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createServer(page =>
+    createInertiaApp({
+        page,
+        title: title => `${title} - ${appName}`,
+        render: ReactDOMServer.renderToString,
+        resolve: name => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/**/*.jsx')),
+        setup: ({ App, props }) => <App {...props} />,
+    }),
+)
