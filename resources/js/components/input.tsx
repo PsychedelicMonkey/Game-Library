87ccd14159a5059e60406b052c7412b31952c9c7
PsychedelicMonkey <ryan.getz.96@gmail.com>
2025-06-15 15:31:45 -0600

    Input variants

--- resources/js/components/input.tsx
+++ resources/js/components/input.tsx
@@ -1,6 +1,29 @@
 import { cn } from '@/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
 import React from 'react';
 
-export default function Input({ className = '', type, ...props }: React.ComponentProps<'input'>) {
-    return <input type={type} className={cn('input', className)} {...props} />;
+const inputVariants = cva('input', {
+    variants: {
+        color: {
+            neutral: 'input-neutral',
+            primary: 'input-primary',
+            secondary: 'input-secondary',
+            accent: 'input-accent',
+            info: 'input-info',
+            success: 'input-success',
+            warning: 'input-warning',
+            error: 'input-error',
+        },
+        size: {
+            xs: 'input-xs',
+            sm: 'input-sm',
+            md: 'input-md',
+            lg: 'input-lg',
+            xl: 'input-xl',
+        },
+    },
+});
+
+export default function Input({ className, color, type, ...props }: React.ComponentProps<'input'> & VariantProps<typeof inputVariants>) {
+    return <input type={type} className={cn(inputVariants({ color, className }))} {...props} />;
 }
